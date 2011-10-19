70cc9d6584923ff2025a476c049cd95d0304676f
Oskari Pirttikoski <oskari.pirttikoski@reaktor.fi>
2011-10-19 11:00:25 +0300

    add Robospecs spec

--- project/build.scala
+++ project/build.scala
@@ -19,19 +19,33 @@ object General {
       keyalias in Android := "change-me",
       libraryDependencies += "org.scalatest" %% "scalatest" % "1.5.1" % "test"
     )
+
+  lazy val testSettings =
+    Seq (
+      resolvers ++= Seq("snapshots" at "http://scala-tools.org/repo-snapshots",
+        "releases"  at "http://scala-tools.org/repo-releases"),
+
+      libraryDependencies ++= Seq(
+          // with Scala 2.8.1 (specs2 1.5 is the latest version for scala 2.8.1)
+          "org.specs2" %% "specs2" % "1.5",
+          "org.specs2" %% "specs2-scalaz-core" % "5.1-SNAPSHOT" % "test"
+      ),
+    resolvers ++= Seq("snapshots" at "http://jbrechtel.github.com/repo/snapshots"),
+    libraryDependencies += "com.github.jbrechtel" %% "robospecs" % "0.1-SNAPSHOT" % "test"
+  )
 }
 
 object AndroidBuild extends Build {
   lazy val main = Project (
     "Easy Bar Code",
     file("."),
-    settings = General.fullAndroidSettings
+    settings = General.fullAndroidSettings ++ General.testSettings
   )
 
   lazy val tests = Project (
     "tests",
     file("tests"),
-    settings = General.settings ++ AndroidTest.androidSettings
+    settings = General.settings ++ AndroidTest.androidSettings ++ General.testSettings
   ) dependsOn main
 
 }
--- src/test/scala/Specs.scala
+++ src/test/scala/Specs.scala
@@ -1,9 +1,20 @@
-import org.scalatest.matchers.ShouldMatchers
-import org.scalatest.Spec
+import net.barcode.BarCodeActivity
+import com.github.jbrechtel.robospecs.RoboSpecs
+import org.specs2.execute.StandardResults._
+import org.specs2.mock.Mockito
+import org.specs2.mutable._
+import org.specs2.matcher.{Expectable, Matcher}
+import org.specs2.specification.BeforeExample
+import com.github.jbrechtel.robospecs.RoboSpecs
+import com.xtremelabs.robolectric.shadows._
 
-class Specs extends Spec with ShouldMatchers {
-  describe("a spec") {
-    it("should do something") {
+class MainActivitySpecs extends RoboSpecs with Mockito {
+
+  "onCreate" should {
+    "not crash" in {
+      val activity = new BarCodeActivity()
+      activity.onCreate(null)
+      done
     }
   }
 }
