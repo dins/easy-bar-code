a06225679429316a607a28463fb9472d206d541e
Oskari Pirttikoski <oskari.pirttikoski@reaktor.fi>
2011-10-21 10:26:18 +0300

    use declarative layout (xml) and add button for starting the scan activity

--- src/main/scala/Activity.scala
+++ src/main/scala/Activity.scala
@@ -2,25 +2,36 @@ package net.barcode
 
 import _root_.android.app.Activity
 import _root_.android.os.Bundle
-import _root_.android.widget.TextView
-import android.content.Intent
+import android.widget.{Button, TextView}
+import android.view.View
+import android.content.{Context, Intent}
 
-class BarCodeActivity extends Activity {
+class BarCodeActivity extends Activity with TypedActivity {
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
+    setContentView(R.layout.main)
+    val button = findView(TR.button)
+    button.setOnClickListener(new ReadButtonListener(this))
+  }
+  def setText(text: String) {
     setContentView(new TextView(this) {
-      setText("I read bar codes!")
+      setText(text)
     })
-    val intent = new Intent("com.google.zxing.client.android.SCAN")
-    //intent.putExtra("SCAN_MODE", "ONE_D_MODE")
-    intent.putExtra("SCAN_FORMATS", "CODE_128")
+  }
+}
 
-    startActivityForResult(intent, 0)
+class ReadButtonListener(activity : BarCodeActivity) extends View.OnClickListener {
+  def onClick(v : View) {
+    val intent = new Intent("com.google.zxing.client.android.SCAN")
+    intent.putExtra("SCAN_MODE", "ONE_D_MODE")
+    try {
+      activity.startActivityForResult(intent, 0)
+    } catch {
+      case ex: Exception => activity.setText("Error: " + ex.getMessage)
+    }
   }
 
-  override def onActivityResult(requestCode: Int, resultCode: Int,  intent: Intent) {
-    setContentView(new TextView(this) {
-      setText("Result: " + intent.getStringExtra("SCAN_RESULT"))
-    })
+  def onActivityResult(requestCode: Int, resultCode: Int,  intent: Intent) {
+    setText("Result: " + intent.getStringExtra("SCAN_RESULT"))
   }
 }
