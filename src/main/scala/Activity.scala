a2f75c3b2bb3952216324759b70ea18c37c14307
Oskari Pirttikoski <oskari.pirttikoski@reaktor.fi>
2011-10-28 14:03:19 +0300

    add read button listener in xml and implement it more conveniently in activity

--- src/main/scala/Activity.scala
+++ src/main/scala/Activity.scala
@@ -15,14 +15,15 @@ class BarCodeActivity extends Activity with TypedActivity {
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
-    val button = findView(TR.button)
-    button.setOnClickListener(new ReadButtonListener(this))
     setMessage("Email: " + getEmailPreference)
     checkEmailIsSet()
     findView(TR.send_button).setEnabled(false)
   }
+  def startScanButtonOnClick(view: View) {
+    startScan
+  }
   def sendButtonOnClick(view: View) {
-    sendEmail()
+    sendEmail
   }
   override def onCreateOptionsMenu(menu: Menu): Boolean = {
     getMenuInflater().inflate(R.menu.menu, menu)
@@ -98,7 +99,18 @@ class BarCodeActivity extends Activity with TypedActivity {
       setText("Result: " + code + ", " + extras.toString)
     })
   }
-  private def sendEmail() {
+
+  private def startScan {
+    val intent = new Intent("com.google.zxing.client.android.SCAN")
+    intent.putExtra("SCAN_MODE", "ONE_D_MODE")
+    try {
+      startActivityForResult(intent, 0)
+    } catch {
+      case ex: Exception => setMessage("Error: " + ex.getMessage)
+    }
+  }
+
+  private def sendEmail {
     val intent = new Intent(Intent.ACTION_SEND)
     intent.setType("plain/text")
     intent.putExtra(Intent.EXTRA_EMAIL, getEmailPreference())
@@ -123,19 +135,6 @@ class BarCodeActivity extends Activity with TypedActivity {
     intent.putExtra("EXTRA_SUBJECT", "A bill to pay")
     startActivity(intent)
   }
-}
-
-class ReadButtonListener(activity : BarCodeActivity) extends View.OnClickListener {
-  def onClick(v : View) {
-    val intent = new Intent("com.google.zxing.client.android.SCAN")
-    intent.putExtra("SCAN_MODE", "ONE_D_MODE")
-    try {
-      activity.startActivityForResult(intent, 0)
-    } catch {
-      case ex: Exception => activity.setMessage("Error: " + ex.getMessage)
-    }
-  }
-
 }
 
 class Preferences extends PreferenceActivity {
